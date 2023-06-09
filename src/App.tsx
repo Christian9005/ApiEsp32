30f02cb35c50d690dbe607e55edcafac7576f131
Christian Albarracin <christianalex-071@hotmail.com>
2023-06-08 23:54:19 -0500

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ScreenDivisions from "./components/atoms/screen-divisions/screen-divisions";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <ScreenDivisions/>
       </header>
     </div>
   );
---
+++ src/components/atoms/screen-divisions/screen-divisions.tsx
@@ -0,0 +1,73 @@
+import React, { FC, useEffect, useState } from 'react';
+import axios from 'axios';
+import './screen-divisions.scss';
+
+interface Zona {
+    id: number;
+    nombre: string;
+    datos: number[];
+}
+
+const ScreenDivisions: FC = () => {
+    const [zonas, setZonas] = useState<Zona[] | null>(null);
+
+    useEffect(() => {
+        // Función para obtener los datos de la API
+        const fetchData = async () => {
+            try {
+                const response = await axios.get<Zona[]>('https://esp32api.azurewebsites.net/api/Zona');
+                const data = response.data;
+                setZonas(data);
+            } catch (error) {
+                console.error('Error al obtener los datos de la API: ', error);
+            }
+        };
+
+        // Llamada inicial a la API
+        fetchData();
+
+        // Actualizar los datos cada 5 segundos (ajusta el intervalo según tus necesidades)
+        const interval = setInterval(fetchData, 500);
+
+        // Limpieza del intervalo al desmontar el componente
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="container">
+            <h2 className="tittle">Ubicación Zonal de Órdenes</h2>
+            <div className="row">
+                {zonas &&
+                    zonas.slice(0, 2).map((zona, index) => (
+                        <div key={zona.id} className={`section section--${index % 2 === 0 ? 'primary' : 'secondary'}`}>
+                            <p className="zone-title">{zona.nombre}</p>
+                            <ul className="data">
+                                {zona.datos.map((valor, index) => (
+                                    <li key={index} className="data-item">
+                                        {valor},
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+            </div>
+            <div className="row">
+                {zonas &&
+                    zonas.slice(-2).map((zona, index) => (
+                        <div key={zona.id} className={`section section--${index % 2 === 0 ? 'gray' : 'cyan'}`}>
+                            <p className="zone-title">{zona.nombre}</p>
+                            <ul className="data">
+                                {zona.datos.map((valor, index) => (
+                                    <li key={index} className="data-item">
+                                        {valor},
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    );
+};
+
+export default ScreenDivisions;
